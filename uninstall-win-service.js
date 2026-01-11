f51c1797f4e55cc5475db6e995e7b24250f6026d
lisong <409051987@qq.com>
2026-01-11 19:19:53 +0800

    test

--- uninstall-win-service.js
+++ uninstall-win-service.js
@@ -1,7 +1,7 @@
 const sudo = require('sudo-prompt')
 
 const serviceName = 'ListenTunService'
-const command = `sc stop ${serviceName} & sc delete ${serviceName}`
+const command = `sc stop ${serviceName} & sc delete ${serviceName} & sc stop ${serviceName}_DNS & sc delete ${serviceName}_DNS`
 
 sudo.exec(command, { name: 'Uninstall Tun Service' }, (err, stdout, stderr) => {
   console.log('err:', err)
